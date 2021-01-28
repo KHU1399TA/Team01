e3b9e29d2a22bf570374762e47ae3da07b652891
Ecstatic7 <nima.nilchian@gmail.com>
2021-01-28 23:47:52 +0330

    cleaned cashier

--- src/main/Cashier.java
+++ src/main/Cashier.java
@@ -4,43 +4,49 @@ import java.util.Date;
 
 public class Cashier extends User {
 
-	public Cashier(String firstName, String lastName, String phoneNumber, String username, String password,
-			AccessLevel accessLevel, Date registrationDate, Date lastLoginDate) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.phoneNumber = phoneNumber;
-		this.username = username;
-		this.password = password;
-		this.accessLevel = accessLevel;
-		this.registrationDate = registrationDate;
-		this.lastLoginDate = lastLoginDate;
-	}
+    public Cashier(String firstName, String lastName, String phoneNumber, String username, String password,
+                   AccessLevel accessLevel, Date registrationDate, Date lastLoginDate) {
 
-	Resault ConfirmOrder(String id, Restaurant restaurant) {
-		Resault resault = new Resault();
-		int foodId = 0;
-		resault.restaurant = restaurant;
-		for (int i = 0; i < restaurant.orders.size(); i++) {
-			if (restaurant.orders.get(i).id.equals(id)) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.username = username;
+        this.password = password;
+        this.accessLevel = accessLevel;
+        this.registrationDate = registrationDate;
+        this.lastLoginDate = lastLoginDate;
+    }
 
-				foodId = restaurant.orders.get(i).foodId;
+    Resault ConfirmOrder(String id, Restaurant restaurant) {
+        Resault resault = new Resault();
+        resault.restaurant = restaurant;
 
-				for (int j = 0; j < restaurant.menu.size(); j++) {
-					if (restaurant.menu.get(j).id == foodId) {
-						if (restaurant.menu.get(i).isAvailable) {
-							restaurant.orders.get(i).state = OrderState.CONFIRMED;
-							resault.restaurant = restaurant;
-							resault.actionResult = ActionResult.SUCCESS;
-							return resault;
-						} else {
-							resault.actionResult = ActionResult.FOOD_NOT_AVAILABLE;
-							return resault;
-						}
-					}
-				}
-			}
-		}
-		resault.actionResult = ActionResult.ORDER_NOT_FOUND;
-		return resault;
-	}
+        int foodId;
+
+        for (int i = 0; i < restaurant.orders.size(); i++) {
+            if (restaurant.orders.get(i).id.equals(id)) {
+
+                foodId = restaurant.orders.get(i).foodId;
+
+                for (int j = 0; j < restaurant.menu.size(); j++) {
+                    if (restaurant.menu.get(j).id == foodId) {
+                        if (restaurant.menu.get(i).isAvailable) {
+
+                            restaurant.orders.get(i).state = OrderState.CONFIRMED;
+
+                            resault.restaurant = restaurant;
+                            resault.actionResult = ActionResult.SUCCESS;
+
+                            return resault;
+                        } else {
+                            resault.actionResult = ActionResult.FOOD_NOT_AVAILABLE;
+                            return resault;
+                        }
+                    }
+                }
+            }
+        }
+        resault.actionResult = ActionResult.ORDER_NOT_FOUND;
+        return resault;
+    }
 }
